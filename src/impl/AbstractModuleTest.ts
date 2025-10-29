caba819fb43da92102531ed6f5b0d29d0a55c146
ericthecurious <hello@ericthecurious.com>
2025-10-28 18:42:05 -0700

    patch: add type to AbstractModuleTest

--- src/impl/AbstractModuleTest.ts
+++ src/impl/AbstractModuleTest.ts
@@ -1,6 +1,9 @@
 import { createRequire } from 'module'
+
 const require = createRequire(import.meta.url)
 
-const { default: AbstractSpruceTest } = require('@sprucelabs/test-utils')
+const { default: AbstractSpruceTest } = require('@sprucelabs/test-utils') as {
+    default: (typeof import('@sprucelabs/test-utils'))['default']
+}
 
 export default class AbstractModuleTest extends AbstractSpruceTest {}

7862e48be0211a5285218df7fcf5f26562d5ced1
ericthecurious <hello@ericthecurious.com>
2025-10-28 17:47:37 -0700

    patch: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-//exports go here
+export { default as AbstractModuleTest } from './impl/AbstractModuleTest.js'
+export * from './impl/AbstractModuleTest.js'
+
+export { default as test } from './functions/test.js'
+export * from './functions/test.js'
+
+export { default as assert } from './functions/assert.js'
+export * from './functions/assert.js'
